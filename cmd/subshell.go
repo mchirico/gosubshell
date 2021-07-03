50f3099031cef813a4b013ce9ce25c1eb13d2cc7
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-07-03 09:30:38 -0400

    update-automated-save

--- cmd/subshell.go
+++ cmd/subshell.go
@@ -31,12 +31,10 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: shell.CreateSubShell,
-
 }
 
 func init() {
 	rootCmd.AddCommand(subshellCmd)
-
 
 	// Here you will define your flags and configuration settings.
 
